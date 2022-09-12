48e9e718eed7c4da7425bca5ffb51a0b259dc61c
TheTeaCup <lw0482352@gmail.com>
2022-09-11 22:58:22 -0400

    theme fixes

--- utils/theme.js
+++ utils/theme.js
@@ -1,9 +1,17 @@
 import {extendTheme} from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools';
 
 const theme = extendTheme({
     config: {
         initialColorMode: 'dark',
         useSystemColorMode: false,
+    },
+    styles: {
+        global: (props) => ({
+            body: {
+                bg: mode('gray.50', 'gray.900')(props),
+            }
+        })
     }
 })
 
