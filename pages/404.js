5d857f8a3848c3959ccbd5ee87fec1ed1e340248
TheTeaCup <lw0482352@gmail.com>
2022-09-20 21:46:03 -0400

    code formatting

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import NavBar_Home from "../components/navbar/home";
-import { Button, Heading, Text, VStack } from '@chakra-ui/react'
+import {Button, Heading, Text, VStack} from '@chakra-ui/react'
 import NextLink from 'next/link'
-import { FaHome } from 'react-icons/fa'
+import {FaHome} from 'react-icons/fa'
 
 function NotFoundPage() {
 
@@ -22,12 +22,12 @@ function NotFoundPage() {
                 textAlign='center'
             >
                 <Heading>404 | Page Not Found</Heading>
-                <Text fontSize={{ md: 'xl' }}>You just hit a route that doesn&apos;t exist... the sadness.😢</Text>
+                <Text fontSize={{md: 'xl'}}>You just hit a route that doesn&apos;t exist... the sadness.😢</Text>
                 <NextLink href='/' passHref>
                     <Button
                         as='a'
                         aria-label='Back to Home'
-                        leftIcon={<FaHome />}
+                        leftIcon={<FaHome/>}
                         colorScheme='teal'
                         size='lg'
                     >
@@ -39,4 +39,4 @@ function NotFoundPage() {
     )
 }
 
-export default NotFoundPage ;
+export default NotFoundPage;
