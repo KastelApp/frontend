a7ee023a51bd4cee305ed59e3c5015c28d19310e
TheTeaCup <lw0482352@gmail.com>
2022-09-28 21:13:17 -0400

    Branding page (WIP)

---
+++ pages/branding.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import NavBar_Home from "../components/navbar/home";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+import {getCookie} from "cookies-next";
+
+function HomePage({user}) {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.prefetch('/login')
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <title>Kastel - Branding</title>
+            </Head>
+
+            <NavBar_Home user={user}/>
+        </>
+    )
+}
+
+export const getServerSideProps = ({req, res}) => {
+    return {
+        props: {
+            user: getCookie('user', {req, res}) || null,
+        }
+    };
+};
+
+export default HomePage;
