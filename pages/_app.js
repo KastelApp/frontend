dfaa33d684a0346eab561cc4558e1a8ba3008a9d
TheTeaCup <lw0482352@gmail.com>
2023-05-18 20:56:14 -0400

    removing some unnecessary things

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,8 @@
 import {ChakraProvider} from '@chakra-ui/react'
 import theme from "../utils/theme";
-import NProgress from 'nprogress';
-import {debounce} from 'lodash';
-import RouterEvents from "../utils/router-events";
 import {useState} from "react";
 import '../styles/globals.css';
 import Head from 'next/head';
-
-const start = debounce(NProgress.start, 100);
-RouterEvents.on('routeChangeStart', start);
-RouterEvents.on('routeChangeComplete', (url) => {
-    console.log(`Changed to URL: ${url}`);
-    start.cancel();
-    NProgress.done();
-});
-RouterEvents.on('routeChangeError', () => {
-    start.cancel();
-    NProgress.done();
-});
 
 function MyApp({Component, pageProps}) {
 
---
+++ pages/verification.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import NavBar_Home from "../components/navbar/home";
+import {getCookie} from "cookies-next";
+import Script from "next/script";
+
+function HomePage({user}) {
+
+
+    return (
+        <>
+            {process.env.NODE_ENV === 'production' && (
+                <Script async defer data-website-id="83dba013-b17b-45c2-a9bd-9f81d2a63c1c"
+                        src="https://analytics.kastelapp.com/umami.js"></Script>
+            )}
+            <Head>
+                <title>Kastel</title>
+            </Head>
+
+            <NavBar_Home user={user}/>
+
+
+
+        </>
+    )
+}
+
+export const getServerSideProps = ({req, res}) => {
+    return {
+        props: {
+            user: getCookie('token', {req, res}) || null,
+        }
+    };
+};
+
+export default HomePage;
