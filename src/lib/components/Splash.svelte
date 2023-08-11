d2caed571c4124f69ff449ae408f1f8135f83f4b
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-08-11 22:51:05 +0200

    Fix splash

--- src/lib/components/Splash.svelte
+++ src/lib/components/Splash.svelte
@@ -1,5 +1,19 @@
 <script>
 	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	import { ready } from '$lib/stores.js';
+	import { initClient } from '$lib/client';
+	import { goto } from '$app/navigation';
+	let clientReady = false;
+	let client;
+	ready.subscribe((value) => {
+		clientReady = value;
+		if (value) {
+			client = initClient();
+		}
+	});
+
+	let token;
 
 	let quotes = [
 		'Insert random quote here - Dev',
@@ -29,6 +43,28 @@
 	let quote2 = quotes[Math.floor(Math.random() * quotes.length)];
 	let active = 1;
 	let show = true;
+	let mounted = false;
+
+	onMount(() => {
+		if (!clientReady) {
+			token =
+				document?.cookie
+					?.split(';')
+					.find((c) => c.startsWith('token'))
+					?.split('=')[1] || '';
+
+			client = initClient(token || '');
+		} else {
+		}
+
+		client?.on('ready', () => {
+			show = false;
+		});
+		client?.on('unready', () => {
+			show = true;
+			goto('/login');
+		});
+	});
 
 	setInterval(() => {
 		if (active == 1) {
@@ -39,22 +75,40 @@
 			quote2 = quotes[Math.floor(Math.random() * quotes.length)];
 		}
 	}, 3000);
+	onMount(() => {
+		mounted = true;
+	});
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center bg-black w-full h-full">
-	<div class="flex flex-col items-center">
-		<img src="/logo.png" alt="The Kastel Logo" />
-		<div class="quotecontainer">
-			{#if active === 1}
-				<div class="text-white text-2xl font-bold mt-4 text-center" out:fade in:fade>{quote1}</div>
-			{:else if active === 2}
-				<div class="text-white text-2xl font-bold mt-4 text-center" in:fade out:fade>{quote2}</div>
+{#if show}
+	<div
+		class="fixed inset-0 flex items-center justify-center w-full h-full splash"
+		out:fade={{ duration: 200 }}
+	>
+		<div class="flex flex-col items-center">
+			<img src="/logo.png" alt="The Kastel Logo" />
+			<div class="quotecontainer">
+				{#if active === 1}
+					<div class="text-white text-2xl font-bold mt-4 text-center" out:fade in:fade>
+						{quote1}
+					</div>
+				{:else if active === 2}
+					<div class="text-white text-2xl font-bold mt-4 text-center" in:fade out:fade>
+						{quote2}
+					</div>
+				{/if}
+			</div>
+			{#if mounted}
+				<div class="text-white text-xl mt-4 text-center" in:fade={{ delay: 10000 }} out:fade>
+					It looks like we're taking a while to connect to our servers. They might be down.
+				</div>
 			{/if}
 		</div>
 	</div>
-</div>
+{/if}
 
 <style>
+	/*weird hacky fix to make the text fade in and out*/
 	.quotecontainer {
 		display: grid;
 		grid-template-rows: 1fr;
@@ -64,5 +118,13 @@
 	.quotecontainer > div {
 		grid-row: 1;
 		grid-column: 1;
+	}
+
+	.splash {
+		background-image: url('/splash.svg');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		background-color: #161922;
 	}
 </style>
