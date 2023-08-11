ada5076af9e8e115da75fb3ba3874c4b2ca6a8dc
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-08-11 22:51:37 +0200

    welcome text

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,1 +1,21 @@
-hi
+<script>
+	let client;
+	let user;
+	import { ready } from '$lib/stores.js';
+	import { t } from '$lib/translations';
+	import { initClient } from '$lib/client';
+	let clientReady = false;
+	ready.subscribe((value) => {
+		clientReady = value;
+		if (value) {
+			client = initClient();
+
+			//Set user
+			user = client?.users?.getCurrentUser();
+		}
+	});
+</script>
+
+{#if clientReady}
+	{$t('common.welcome', { name: user?.username })}
+{/if}
