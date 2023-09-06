98c9bc61fa9fe2684e82d9728e9b40c744a714b1
Darker-Ink <rstonehouse66@gmail.com>
2023-09-05 19:22:26 -0500

    smol updates

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,35 +1,44 @@
-import {env} from '$env/dynamic/public';
+import { env } from '$env/dynamic/public';
 import { Client } from '@kastelll/wrapper';
 import { ready } from './stores.js';
 
+/**
+ * @type {Client}
+ */
 let client;
-function initClient(token){
-    if(!token) token = "none";
-    if(typeof window === 'undefined') return null;
-    if(client) return client;
-      
-    
 
-      client = new Client({
-        apiUrl: env.PUBLIC_API_URL,
-        token: token,
-        version: env.PUBLIC_API_VERSION,
-        wsUrl: env.PUBLIC_API_WS_URL,
-        worker: new Worker('/workers/interval.worker.js')
-      });
+function initClient(token) {
+  if (typeof window === 'undefined') return null;
+  if (client) return client;
 
-      //TODO: change to unauthed when fixed
-      client.on('unready', () => {
-        ready.set(false);
-      });
-      client.on('ready', () => {
-        ready.set(true);
-      });
+  client = new Client({
+    apiUrl: env.PUBLIC_API_URL,
+    token: token ?? null,
+    version: env.PUBLIC_API_VERSION,
+    wsUrl: env.PUBLIC_API_WS_URL,
+    worker: new Worker('/workers/interval.worker.js'),
+    unAuthed: token ? false : true,
+  });
 
-      client.connect();      
-  
-      return client;
-    }   
+  client.on('unReady', () => {
+    setTimeout(() => {
+      ready.set(false);
+    }, 2500); // in case we can reconnect in 2.5 seconds, don't show the loading screen (cache will be caught up)
+  });
+
+  // you should redirect to login here
+  client.on('unAuthed', () => {
+    ready.set(false);
+  });
+
+  client.on('ready', () => {
+    ready.set(true);
+  });
+
+  client.connect();
+
+  return client;
+}
 
 
-export {initClient}
+export { initClient };
