43b9a5046bcac59e1ecc01e0b85ee169a9be1ff8
TheTeaCup <lw0482352@gmail.com>
2023-10-13 23:01:40 -0400

    Login page updates

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-	import {browser} from '$app/environment';
-	import {initClient} from '$lib/client';
-	import {fade} from 'svelte/transition';
-	import {ready, token} from '$lib/stores.js';
-	import {t} from '$lib/translations';
-	import {goto} from '$app/navigation';
+    import {browser} from '$app/environment';
+    import {initClient} from '$lib/client';
+    import {fade} from 'svelte/transition';
+    import {ready, token} from '$lib/stores.js';
+    import {t} from '$lib/translations';
+    import {goto} from '$app/navigation';
 
-	/**
+    /**
      * @type {import('@kastelll/wrapper').Client}
      */
     let client;
 
-	ready.subscribe((value) => {
+    ready.subscribe((value) => {
         if ($token) {
             if (browser) {
                 goto('/app');
@@ -25,98 +25,128 @@
         message: 'Invalid email or password',
         shown: false
     };
+
+    function onSubmit(e) {
+        console.log(e.target);
+        const email = e.target.input['email'].value;
+        const password = e.target.input['password'].value;
+
+        if (!client.EmailRegex.test(email)) {
+            error = {
+                message: 'Invalid email',
+                shown: true
+            };
+
+            return;
+        } else if (!client.PasswordRegex.test(password)) {
+            error = {
+                message: 'Invalid password',
+                shown: true
+            };
+
+            return;
+        } else {
+            error = {
+                message: 'N/A',
+                shown: false
+            };
+        }
+
+        client.loginAccount({email, password, resetClient: true}).then((data) => {
+            if (data.success) {
+                token.set(data.token);
+
+                client.connect();
+
+                goto('/app');
+
+                return;
+            }
+
+            for (const [key, value] of Object.entries(data.errors)) {
+                if (value) {
+                    error.message = `${error.message === 'N/A' ? '' : error.message}\n${key} is invalid`;
+                }
+            }
+
+            if (error.message !== 'N/A') {
+                error.shown = true;
+
+            } else {
+                error.message = 'Unknown error, please try again later';
+                error.shown = true;
+            }
+        });
+    }
 </script>
 
 <div
-        class="fixed inset-0 flex items-center justify-center w-full h-full splash"
+        class="fixed inset-0 flex items-center justify-center w-full splash"
         out:fade={{ duration: 200 }}
 >
     <form class="flex flex-col items-center">
-        <img alt="Logo" src="/logo.png"/>
+        <p class="mb-5 font-sans text-6xl font-bold text-left text-white/[0.92]">
+            Login to Kastel!
+        </p>
 
-        {#if error.shown}
-            <div class="mb-2 bg-red-500 text-white p-2 rounded-lg">
-                {error.message}
+        <div class="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-[#2d3748]">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                {#if error.shown}
+                    <div class="bg-red-500 text-white p-2 rounded-lg">
+                        {error.message}
+                    </div>
+                {/if}
+
+                <form class="space-y-4 md:space-y-6" on:submit|preventDefault={onSubmit}>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                               for="email">{$t('common.email')}</label>
+                        <input class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               id="email"
+                               name="email"
+                               required
+                               type="email">
+                    </div>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                               for="password">{$t('common.password')}</label>
+                        <input class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               id="password"
+                               name="password"
+                               placeholder="••••••••"
+                               required
+                               type="password">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input aria-describedby="remember"
+                                       class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                       id="remember"
+                                       type="checkbox">
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label class="text-gray-500 dark:text-gray-300" for="remember">Remember me</label>
+                            </div>
+                        </div>
+                        <a class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                           href="/forgot-password">Forgot password?</a>
+                    </div>
+                    <button class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                            type="submit"
+                    >
+                        Sign in
+                    </button>
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Don’t have an account yet? <a
+                            class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            href="/sign-up">Sign
+                        up</a>
+                    </p>
+                </form>
             </div>
-            <br/>
-        {/if}
-        <input class="mb-4 input text-center" placeholder={$t('common.email')} type="email"/>
-		<br/><br/>
-        <input class="input text-center" placeholder={$t('common.password')} type="password"/>
-        <button
-                class="btn variant-filled-secondary w-full mt-5"
-                on:click={async (e) => {
-				const email = e.target.form[0].value;
-				const password = e.target.form[1].value;
+        </div>
 
-				if (!email || !password) {
-					error = {
-						message: 'Please fill in all fields',
-						shown: true
-					};
-
-					return;
-				} else
-
-				if (!client.EmailRegex.test(email)) {
-					error = {
-						message: 'Invalid email',
-						shown: true
-					};
-
-					return;
-				} else if (!client.PasswordRegex.test(password)) {
-					error = {
-						message: 'Invalid password',
-						shown: true
-					};
-
-					return;
-				} else {
-					error = {
-						message: 'N/A',
-						shown: false
-					};
-				}
-
-				const data = await client.loginAccount({ email, password, resetClient: true });
-
-				if (data.success) {
-					token.set(data.token);
-
-					client.connect();
-
-					goto('/app');
-
-					return;
-				}
-
-				for (const [key, value] of Object.entries(data.errors)) {
-					if (value) {
-						error.message = `${error.message === 'N/A' ? '' : error.message}\n${key} is invalid`;
-					}
-				}
-
-				if (error.message !== 'N/A') {
-					error.shown = true;
-
-				} else {
-					error.message = 'Unknown error, please try again later';
-					error.shown = true;
-				}
-			}}
-                type="button"
-        >
-            {$t('common.signIn')}
-		</button>
-
-		<button
-				class="btn variant-filled-tertiary w-full mt-5"
-				on:click={async () => {
-					goto('/new-account')
-				}}>
-			{$t('common.createAcc')}
-		</button>
 
     </form>
 </div>
