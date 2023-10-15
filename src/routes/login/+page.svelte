7187f3faa421bfd5124ac97491889923bfbee2f3
TheTeaCup <lw0482352@gmail.com>
2023-10-15 17:10:46 -0400

    Disable 'remember me' button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -123,7 +123,8 @@
                                 <input aria-describedby="remember"
                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
                                        id="remember"
-                                       type="checkbox">
+                                       type="checkbox"
+                                       disabled>
                             </div>
                             <div class="ml-3 text-sm">
                                 <label class="text-gray-500 dark:text-gray-300" for="remember">Remember me</label>
