9fbf3f400452c03d3d6f1615df40d49635b1d346
TheTeaCup <lw0482352@gmail.com>
2023-10-15 17:14:29 -0400

    Translation update for login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -131,7 +131,7 @@
                             </div>
                         </div>
                         <a class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                           href="/forgot-password">Forgot password?</a>
+                           href="/forgot-password">{$t('common.forgotPass')}</a>
                     </div>
                     <button class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                             type="submit"
