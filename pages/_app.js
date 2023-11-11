c310b98d38c8b845db7d7aea4bb65b82b1348ad6
TheTeaCup <lw0482352@gmail.com>
2023-11-11 16:05:36 -0500

    Setup state manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { appWithTranslation } from 'next-i18next'
+import {RecoilRoot} from "recoil";
+import {ChakraProvider} from '@chakra-ui/react'
+import {appWithTranslation} from 'next-i18next'
 import {DefaultSeo} from "next-seo";
 import '@/styles/globals.css'
 import SEO from "@/components/seo";
 import theme from "@/utils/theme";
 
-function App({ Component, pageProps }) {
-  return (
-      <>
-          <DefaultSeo {...SEO}/>
-          <ChakraProvider theme={theme}>
-              <Component {...pageProps} />
-          </ChakraProvider>
-      </>
-  )
+function App({Component, pageProps}) {
+    return (
+        <>
+            <RecoilRoot>
+                <DefaultSeo {...SEO}/>
+                <ChakraProvider theme={theme}>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            </RecoilRoot>
+        </>
+    )
 }
 
 export default appWithTranslation(App)
