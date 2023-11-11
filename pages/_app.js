bf3f1e08655f0b08260bf23978388af085299740
TheTeaCup <lw0482352@gmail.com>
2023-11-11 02:27:33 -0500

    Add chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
+
+export default App;
