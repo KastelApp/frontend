433b45ab53a357da47b09a26bc45f67759aa0329
TheTeaCup <lw0482352@gmail.com>
2023-11-13 22:14:32 -0500

    Fix for improper error

--- pages/login.js
+++ pages/login.js
@@ -62,6 +62,7 @@ export default function Login() {
                     } else {
                         setError({other: {Message: "Unknown error occurred."}})
                     }
+                    return;
                 }
 
                 setLoading(false);
