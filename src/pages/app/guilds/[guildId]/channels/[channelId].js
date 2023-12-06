9b35dc9edefd0e0f55bcde0a50edcfc4de51e8d9
TheTeaCup <lw0482352@gmail.com>
2023-12-06 16:29:00 -0500

    version 0.0.23: working on guilds

---
+++ src/pages/app/guilds/[guildId]/channels/[channelId].js
@@ -0,0 +1,81 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import {
+  clientStore,
+  guildStore,
+  readyStore,
+  tokenStore,
+} from "@/utils/stores";
+import { Box, Text } from "@chakra-ui/react";
+import AppNavbar from "@/components/app/navbar";
+import Loading from "@/components/app/loading";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import SEO from "@/components/seo";
+
+const GuildChannelPage = () => {
+  const { t } = useTranslation("app");
+  const router = useRouter();
+  const { guildId, channelId } = router.query;
+  const [token] = useRecoilState(tokenStore);
+  const [client] = useRecoilState(clientStore);
+  const [ready] = useRecoilState(readyStore);
+  const [user, setUser] = useState(null);
+  const [guilds] = useRecoilState(guildStore);
+  const [guild, setGuild] = useState(null);
+
+  useEffect(() => {
+    if (!token) return router.push("/login");
+    setUser(client?.users?.getCurrentUser());
+
+    if (ready) {
+      if (guildId) {
+        // is user in guild?
+        if (!guilds.some((guild) => guild.id === guildId)) {
+          router.push("/app");
+        }
+
+        const foundGuild = client.guilds.get(guildId);
+        if (!foundGuild) return router.push("/app");
+        setGuild(foundGuild);
+        console.log(foundGuild);
+
+        // is channel in guild?
+        /*if (!guilds.some((guild) => guild.channels.some((channel) => channel.id === channelId))) {
+                     router.push("/app");
+                }*/
+      } else {
+        router.push("/app");
+      }
+    }
+  }, [guildId, ready]);
+
+  return (
+    <>
+      <SEO title={t("title")} />
+      {ready ? (
+        <>
+          <Box>
+            <AppNavbar userInfo={user} guilds={guilds} />
+            <Text>
+              Name: {guild?.name || "Loading"} <br />
+              ID: {guildId} <br />
+              Channel ID: {channelId}
+            </Text>
+          </Box>
+        </>
+      ) : (
+        <Loading translations={t} />
+      )}
+    </>
+  );
+};
+
+export default GuildChannelPage;
+
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale ?? "en", ["app"])),
+  },
+});
