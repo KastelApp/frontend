27f03b19d1faa82acb9f3be0e8e03c1de82db27a
TheTeaCup <lw0482352@gmail.com>
2023-12-06 00:31:01 -0500

    version 0.0.20: dark/light mode

--- src/components/app/settings/sections/appearance.js
+++ src/components/app/settings/sections/appearance.js
@@ -1,3 +1,19 @@
+import { BsSun, BsMoonStarsFill } from "react-icons/bs"
+import { Button, useColorMode } from "@chakra-ui/react"
+
 export default function Settings_Appearance() {
-  return <div>Appearance</div>;
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+      <>
+        <div>Appearance</div>
+          <br/>
+        <Button
+            aria-label="Toggle Color Mode"
+            onClick={toggleColorMode}
+            _focus={{ boxShadow: "none" }}
+            w="fit-content">
+          {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+        </Button>
+      </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import i18nextConfig from "../../next-i18next.config.js";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "@/utils/theme";
 
 export default function Document(props) {
   const currentLocale =
@@ -9,6 +11,7 @@ export default function Document(props) {
     <Html lang={currentLocale}>
       <Head />
       <body>
+       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
