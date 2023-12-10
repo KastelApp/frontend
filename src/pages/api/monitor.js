0008bc2dcb0f169c600b94992360f11a5ed50a46
TheTeaCup <lw0482352@gmail.com>
2023-12-10 00:26:30 -0500

    version 0.0.46: cloudflare build error fix?

--- src/pages/api/monitor.js
+++ src/pages/api/monitor.js
@@ -1,19 +1,19 @@
-export default async function handler(req, res) {
+export default async function handler(request) {
+    const { method, body } = request;
 
-    if (req.method !== "POST") {
-        res.status(500).send("Invalid request");
-
-        return;
+    if (method !== "POST") {
+        return new Response("Invalid request", { status: 500 });
     }
 
-    const envelope = req.body;
-    const pieces = envelope.split("\n");
-    const header = JSON.parse(pieces[0]);
-    const { host, pathname, username } = new URL(header.dsn);
-    const projectId = pathname.slice(1);
-    const url = `https://${host}/api/${projectId}/envelope/?sentry_key=${username}`;
-
     try {
+        const blob = await (new Blob([await readRequestBody(body)])); // Create a blob from the request body
+        const envelope = await blob.text(); // Read the blob as text
+        const pieces = envelope.split("\n");
+        const header = JSON.parse(pieces[0]);
+        const { host, pathname, username } = new URL(header.dsn);
+        const projectId = pathname.slice(1);
+        const url = `https://${host}/api/${projectId}/envelope/?sentry_key=${username}`;
+
         const response = await fetch(url, {
             headers: {
                 "Content-Type": "application/x-sentry-envelope"
@@ -25,9 +25,36 @@ export default async function handler(req, res) {
         const text = await response.text();
         const data = JSON.parse(text);
 
-        res.status(201).json({ message: "Success", data: data })
+        return new Response(JSON.stringify({ message: "Success", data }), {
+            status: 201,
+            headers: { "Content-Type": "application/json" }
+        });
     } catch (e) {
         const error = e?.response || e?.message;
-        res.status(400).json({ message: "invalid request", error: error });
+        return new Response(JSON.stringify({ message: "Invalid request", error }), {
+            status: 400,
+            headers: { "Content-Type": "application/json" }
+        });
     }
 }
+
+async function readRequestBody(body) {
+    const reader = body.getReader();
+    let decodedData = "";
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const { done, value } = await reader.read();
+
+        if (done) {
+            break;
+        }
+
+        const chunk = new TextDecoder().decode(value);
+        decodedData += chunk;
+    }
+
+    return decodedData;
+}
+
+export const runtime = "edge";
