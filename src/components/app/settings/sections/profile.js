c1cbef7b1b2533d0cf85e5f33d2da4784ade9b40
DarkerInk <rstonehouse66@gmail.com>
2023-12-17 06:50:24 -0600

    avatar uploading (not finished)

--- src/components/app/settings/sections/profile.js
+++ src/components/app/settings/sections/profile.js
@@ -10,13 +10,34 @@ import {
   FormControl,
   FormLabel,
   IconButton,
+  Input,
   Stack,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { SmallCloseIcon } from "@chakra-ui/icons";
+import { useRef, useState } from "react";
 
 export default function Settings_Profile({ userInfo }) {
   // const [client] = useRecoilState(clientStore);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  const handleImageChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    const Hash = await userInfo.uploadAvatar(file);
+
+    console.log(Hash);
+  };
+
 
   return (
     <>
@@ -35,7 +56,21 @@ export default function Settings_Profile({ userInfo }) {
             <FormLabel>User Icon</FormLabel>
             <Stack direction={["column", "row"]} spacing={6}>
               <Center>
-                <Avatar size="xl" src={userInfo?.getAvatarUrl({})}>
+                <Avatar
+                  size="xl"
+                  src={selectedImage || userInfo?.getAvatarUrl({
+                    size: 256,
+                  })}
+                  name="avatar-preview"
+                  mb={4}
+                  cursor="pointer"
+                  onClick={() => {
+                    const input = fileInputRef.current;
+                    if (input) {
+                      input.click();
+                    }
+                  }}
+                >
                   <AvatarBadge
                     as={IconButton}
                     size="sm"
@@ -46,6 +81,21 @@ export default function Settings_Profile({ userInfo }) {
                     icon={<SmallCloseIcon />}
                   />
                 </Avatar>
+                <Input
+                  type="file"
+                  id="avatar-input"
+                  ref={fileInputRef}
+                  onChange={handleImageChange}
+                  accept="image/*"
+                  position="absolute"
+                  top="0"
+                  left="0"
+                  opacity="0"
+                  width="100%"
+                  height="100%"
+                  cursor="pointer"
+                  zIndex="-1"
+                />
               </Center>
               <Center w="full">
                 <Button w="full">Change Icon</Button>
