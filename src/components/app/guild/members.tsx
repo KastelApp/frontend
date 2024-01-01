ed3524d4f86ef3937ae2436c76e62113ea67bfe9
TheTeaCup <lw0482352@gmail.com>
2024-01-01 02:09:05 -0500

    version 0.0.655: guild members

--- src/components/app/guild/members.tsx
+++ src/components/app/guild/members.tsx
@@ -11,60 +11,82 @@ import {
   PopoverTrigger,
   Text,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { GuildMember } from "@kastelll/wrapper";
 
 const GuildMembers = () => {
   const [guild] = useRecoilState(currentGuild);
+  const [members, setMembers] = useState<GuildMember[]>([]);
+
+  useEffect(() => {
+    if (!guild?.members) return;
+
+    setMembers(guild?.members.toArray());
+  }, [guild?.members]);
 
   return guild ? (
     <Box>
-      <Popover placement={"left"}>
-        <PopoverTrigger>
-          <Flex
-            _hover={{
-              bg: "gray.700",
-              rounded: "10px",
-              cursor: "pointer",
-            }}
-            mt={2}
-            ml={2}
-            mr={2}
-          >
-            <Box ml={2} display="flex" alignItems="center" py={1}>
-              <Box
-                boxSize="30px"
-                display="inline-flex"
-                alignItems="center"
-                justifyContent="center"
-                overflow="visible"
-                lineHeight="none"
-                borderRadius="full"
-                position="relative"
+      {members?.map((member, index) => (
+        <Box key={index}>
+          <Popover placement={"left"}>
+            <PopoverTrigger>
+              <Flex
+                _hover={{
+                  bg: "gray.700",
+                  rounded: "10px",
+                  cursor: "pointer",
+                }}
+                mt={2}
+                ml={2}
+                mr={2}
               >
-                <Image
-                  borderRadius={"full"}
-                  src={"/icon-1.png"}
-                  alt={"Loading"}
-                  fit="cover"
-                />
-                <Badge
-                  boxSize="3"
-                  borderRadius="full"
-                  bg={"green"}
-                  position="absolute"
-                  bottom="-0.5"
-                  right="-0.5"
-                />
-              </Box>
-            </Box>
-            <Text ml={2} mt={1}>
-              Testing
-            </Text>
-          </Flex>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverBody>test</PopoverBody>
-        </PopoverContent>
-      </Popover>
+                <Box ml={2} display="flex" alignItems="center" py={1}>
+                  <Box
+                    boxSize="30px"
+                    display="inline-flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    overflow="visible"
+                    lineHeight="none"
+                    borderRadius="full"
+                    position="relative"
+                  >
+                    <Image
+                      borderRadius={"full"}
+                      src={member?.user?.getAvatarUrl({ size: 128 }) ?? ""}
+                      fallbackSrc={"/icon-1.png"}
+                      alt={member?.user?.username || "loading"}
+                      fit="cover"
+                    />
+                    <Badge
+                      boxSize="3"
+                      borderRadius="full"
+                      bg={
+                        member?.user?.presence === "online"
+                          ? "green.500"
+                          : member?.user?.presence === "idle"
+                            ? "yellow.500"
+                            : member?.user?.presence === "dnd"
+                              ? "red.500"
+                              : "gray.500"
+                      }
+                      position="absolute"
+                      bottom="-0.5"
+                      right="-0.5"
+                    />
+                  </Box>
+                </Box>
+                <Text ml={2} mt={1}>
+                  {member?.user?.username}
+                </Text>
+              </Flex>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverBody>coming soon</PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </Box>
+      ))}
     </Box>
   ) : null;
 };
