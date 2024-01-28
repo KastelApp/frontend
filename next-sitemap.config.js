bbd432b165d04ab5e3335be58394c9a079588585
TheTeaCup <lw0482352@gmail.com>
2024-01-28 16:44:02 -0500

    version 0.0.68: setting up site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://kastelapp.com',
     generateRobotsTxt: true,
+    exclude: ['/app/*', '/api/*', '/invite/*', '/oauth2/*', '/test']
 }
