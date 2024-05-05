bda0f0eb528275193c30eb5f6f37f31a8d6130c3
DarkerInk <rstonehouse66@gmail.com>
2024-05-05 05:16:34 -0500

    feat: redirects for playground & temp

--- next.config.js
+++ next.config.js
@@ -9,11 +9,37 @@ const nextConfig = {
         source: "/app/guilds",
         destination: "/app",
       },
-    ]
+    ];
+  },
+  redirects: async () => {
+    // ? There's a few spots we want to redirect, depending on the environment
+    // ? For example, if we are in production we want to redirect /playground to /404 since its a dev only route
+    // ? same with /temp
+
+    /**
+     * @type {import('next').Redirect[]}
+     */
+    const redirects = [];
+
+    if (process.env.NODE_ENV === "production") {
+      redirects.push({
+        source: "/playground/:slug*",
+        destination: "/404",
+        permanent: true,
+      });
+
+      redirects.push({
+        source: "/temp",
+        destination: "/404",
+        permanent: true,
+      });
+    }
+
+    return redirects;
   },
   eslint: {
     ignoreDuringBuilds: true, // todo: remove since this is temp
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
