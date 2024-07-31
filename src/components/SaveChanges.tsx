2ad870c7dd54b14d22d52ab68f21019639065842
Lightning <154468000+LightningDev23@users.noreply.github.com>
2024-07-31 14:00:50 -0400

    Update SaveChanges.tsx
    Grammar fixes

--- src/components/SaveChanges.tsx
+++ src/components/SaveChanges.tsx
@@ -15,7 +15,7 @@ const SaveChanges = ({
             {isShowing && (
                 <div className="bg-neutral-950 w-[85%] sm:w-[75%] h-14 rounded-lg absolute bottom-8 !z-[120] flex items-center justify-center left-[8%] sm:left-[22%]">
                     <div className="flex items-center justify-between w-full px-4">
-                        <p className="text-white text-md font-semibold">Becareful, you have unsaved changes</p>
+                        <p className="text-white text-md font-semibold">Be careful, you have unsaved changes.</p>
                         <div className="flex gap-2">
                             <Button onClick={onCancel} radius="none" variant="flat" color="danger" className="rounded-md h-8">Cancel</Button>
                             <Button onClick={onSave} radius="none" variant="flat" color="success" className="rounded-md h-8">Save Changes</Button>
