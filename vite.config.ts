a60317d708c8e30339bdc0ffa2809fb107dfbda2
DarkerInk <rstonehouse66@gmail.com>
2024-11-11 20:30:13 -0600

    fix: markdown issue

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
+			"@": path.resolve(__dirname, "./src")
 		},
+    dedupe: ["react", "react-dom"],
 	},
 });
